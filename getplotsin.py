ec36393d5a86bc873d40f5548baa4eeca80dd621
Oscar <oscar@Zombie.local>
2026-05-31 13:42:16 +0200

    Modify code, add input of a range (LOL I just did this on a Ryanair plane)

--- getplotsin.py
+++ getplotsin.py
@@ -1,6 +1,15 @@
 # Finds the sin of some numbers and plots them with matplotlib
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
-x = np.arange(0, 20, 0.1)
-y = np.sin(x)
-plt.plot(x, y)
+try:
+	int1 = int(input("Enter a number: "))
+	int2 = int(input("Enter another number: "))
+	print(f"Drawing the sinewave of all numbers between {int1} and {int2}")
+	plt.style.use("ggplot")
+	x = np.arange(int1, int2, 0.1)
+	y = np.sin(x)
+	plt.plot(x, y)
+	plt.show()
+except KeyboardInterrupt:
+	sys.exit(0)
