27fdfe2863ef835c5cd4247b6b5238e32cd9e4dc
Oscar Insh <oscarinsh15@gmail.com>
2026-05-31 14:36:16 +0200

    Add input for the step size (Again I just did this at 35,000 ft in the sky LOL)

--- getplotsin.py
+++ getplotsin.py
@@ -5,9 +5,16 @@ import matplotlib.pyplot as plt
 try:
 	int1 = int(input("Enter a number: "))
 	int2 = int(input("Enter another number: "))
+	stepsize = float(input("Enter the step size: "))
 	print(f"Drawing the sinewave of all numbers between {int1} and {int2}")
 	plt.style.use("ggplot")
-	x = np.arange(int1, int2, 0.1)
+	if int1 < int2:	
+		x = np.arange(int1, int2, stepsize)
+	elif int1 > int2:
+		x = np.arange(int2, int1, stepsize)
+	else:
+		print("Sorry, those 2 numbers cannot be the same. Try again")
+		sys.exit(0)
 	y = np.sin(x)
 	plt.plot(x, y)
 	plt.show()
