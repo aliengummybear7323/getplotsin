58d4f8d3ae452f3594b6e745724242d972096e00
Oscar Insh <oscarinsh15@gmail.com>
2026-06-03 18:54:46 +0100

    Give matplotlib and the macOS backend some relationship therapy (JK just fixing the error handling)

--- getplotsin.py
+++ getplotsin.py
@@ -2,24 +2,31 @@
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+
 try:
-	int1 = float(input("Enter a number: "))
-	int2 = float(input("Enter another number: "))
-	stepsize = float(input("Enter the step size: "))
-	print(f"Drawing the sinewave of all numbers between {int1} and {int2}")
-	plt.style.use("ggplot")
-	if int1 < int2:	
-		x = np.arange(int1, int2, stepsize)
-	elif int1 > int2:
-		x = np.arange(int2, int1, stepsize)
-	else:
-		print("Sorry, those 2 numbers cannot be the same. Try again")
-		sys.exit(0)
-	y = np.sin(x)
-	plt.plot(x, y)
-	plt.show()
+    int1 = float(input("Enter a number: "))
+    int2 = float(input("Enter another number: "))
+    stepsize = float(input("Enter the step size: "))
+    print(f"Drawing the sinewave of all numbers between {int1} and {int2}")
+    plt.style.use("ggplot")
+
+    if int1 < int2:
+        x = np.arange(int1, int2, stepsize)
+    elif int1 > int2:
+        x = np.arange(int2, int1, stepsize)
+    else:
+        print("Sorry, those 2 numbers cannot be the same. Try again")
+        sys.exit(0)
+
+    y = np.sin(x)
+
 except KeyboardInterrupt:
-	print("\nGoodbye...")
-	sys.exit(0)
+    print("\nGoodbye...")
+    sys.exit(0)
 except ValueError:
-	print("Sorry, that is invalid!")
+    print("Sorry, that is invalid!")
+    sys.exit(0)
+
+plt.plot(x, y)
+plt.show()
+
